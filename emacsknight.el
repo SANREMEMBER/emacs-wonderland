939532b1c7466c3874b636d3fbb7528326a0f27a
SANREMEMBER <sanremember@protonmail.com>
2017-10-20 20:00:08 +0700

    cure for icons of airline themes

--- emacsknight.el
+++ emacsknight.el
@@ -100,6 +100,17 @@
   )
 ;; nil to display full dir
 (setq airline-display-directory nil)
+;; Cure for icons in emacs
+(setq powerline-utf-8-separator-left        #xe0b0
+      powerline-utf-8-separator-right       #xe0b2
+      airline-utf-glyph-separator-left      #xe0b0
+      airline-utf-glyph-separator-right     #xe0b2
+      airline-utf-glyph-subseparator-left   #xe0b1
+      airline-utf-glyph-subseparator-right  #xe0b3
+      airline-utf-glyph-branch              #xe0a0
+      airline-utf-glyph-readonly            #xe0a2
+      airline-utf-glyph-linenumber          #xe0a1)
+
 ;; DIMINISH---------------------------------------------------------------
 (require 'diminish)
 (when (require 'diminish nil 'noerror)
