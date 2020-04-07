0e7eafd3ce7d9f0d30820835fa81f2bcb08283f9
SANREMEMBER <sanremember@protonmail.com>
2020-04-07 08:24:47 +0700

    change to https for melpa and org

--- emacsknight.el
+++ emacsknight.el
@@ -12,8 +12,8 @@
 ;; PACKAGE SOURCES
 ;;------------------------------------------------------------------------
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 
 (package-initialize)
 
