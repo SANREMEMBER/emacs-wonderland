f7866d8db6e95518b9ff91d8b6f49ad727ce405a
SANREMEMBER <sanremember@protonmail.com>
2020-05-05 05:21:47 +0700

    :hammer-pick: change py-yapf with yapfify

--- emacsknight.el
+++ emacsknight.el
@@ -41,7 +41,7 @@
      ("account" "%(binary) -f %(ledger-file) reg %(account)"))))
  '(package-selected-packages
    (quote
-    (company php-mode emojify easy-hugo org-fancy-priorities highlight-indent-guides py-yapf engine-mode anaconda-mode yaml-mode ace-window ledger-mode web-beautify emmet-mode rainbow-mode writeroom-mode ox-pandoc pandoc-mode ox-twbs fasd markdown-mode which-key htmlize google-translate deft smex all-the-icons-gnus counsel ivy all-the-icons-dired neotree airline-themes powerline avy diminish evil-magit doom-themes magit evil-leader evil-escape org-bullets evil))))
+    (yapfify company php-mode emojify easy-hugo org-fancy-priorities highlight-indent-guides engine-mode anaconda-mode yaml-mode ace-window ledger-mode web-beautify emmet-mode rainbow-mode writeroom-mode ox-pandoc pandoc-mode ox-twbs fasd markdown-mode which-key htmlize google-translate deft smex all-the-icons-gnus counsel ivy all-the-icons-dired neotree airline-themes powerline avy diminish evil-magit doom-themes magit evil-leader evil-escape org-bullets evil))))
 
 
 
@@ -785,9 +785,9 @@
 (add-to-list 'evil-emacs-state-modes 'anaconda-mode-view-mode)
 
 ;;------------------------------------------------------------------------
-;; PY-YAPF
+;; YAPF
 ;;------------------------------------------------------------------------
-(add-hook 'python-mode-hook 'py-yapf-enable-on-save)
+(add-hook 'python-mode-hook 'yapf-mode)
 
 ;;------------------------------------------------------------------------
 ;; HIGHLIGHT-INDENT-GUIDES
